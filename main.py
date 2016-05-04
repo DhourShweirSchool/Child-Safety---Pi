e2cd3311f4ca3673bb379cda301a410be2f5bd65
DhourShweirSchool <milontoo7@gmail.com>
2016-05-04 22:42:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
-import spidev
+import RPi.GPIO as GPIO
+import dht11
 import time
+import datetime
+import spidev
 import os
 from twilio.rest import TwilioRestClient
-import RPi.GPIO as GPIO
-import commands
 
-
+# initialize GPIO
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
+GPIO.cleanup()
+
+instance = dht11.DHT11(pin = 7)
+
+k = 0
+
+
+def temp():
+    while True:
+        result = instance.read()
+        if result.is_valid():
+            break
+    print("Last valid input: " + str(datetime.datetime.now()))
+    print("Temperature: %d C" % result.temperature )
+
+
+
 GPIO.setup (13, GPIO.OUT)
 GPIO.setup (15, GPIO.OUT)
 
 
 spi = spidev.SpiDev()
 spi.open(0,1)
-output = commands.getoutput("sudo python /home/pi/Adafruit_Python_DHT/examples/AdafruitDHT.py 11 4")
 
+def openwindow():
+    GPIO.output(13,GPIO.HIGH)
+    GPIO.output(15,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(13,GPIO.LOW)
+    GPIO.output(15,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(13,GPIO.LOW)
+    GPIO.output(15,GPIO.LOW)
 
 def readadc(adcnum):
     if((adcnum > 7) or(adcnum < 0)):
@@ -34,20 +61,9 @@ def convertToO2(data):
     value = value / 7.43
     return value
 
-# opening the window
-def openwindow():
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(15,GPIO.HIGH)
-    time.sleep(1)
-    GPIO.output(13,GPIO.HIGH)
-    GPIO.output(15,GPIO.LOW)
-    time.sleep(1)
-    GPIO.output(13,GPIO.LOW)
-    GPIO.output(15,GPIO.LOW)
 
 def sendsms(): 
 # put your own credentials here
-#enter your details of twilio here with the message and number and from
     ACCOUNT_SID = ""
     AUTH_TOKEN = ""
  
@@ -59,28 +75,30 @@ def sendsms():
         body="Oxygen Level is Below 19%",  
 )
 
-s = 0
 
-
-#here is our main loop where we print the percentage of O2 the sensor is give us and the temperature along with the humid when the temp reaches below 19% it will send an sms using function we defined earlier
-
-while 1==1:
-    
-
-    p = readadc(0)
+while True:
+    p = readadc (0)
     v = convertVolts(p)
-    o = convertToO2(v)
+    o =  convertToO2(v)
+    print 'Oxygen Level = ', o
+    temp()
+    result = instance.read()
+    print '------------------------------------------------------------'
 
-
-    print o
-    print output
-    time.sleep(1)
+    
     if o < 19 and s == 0:
         print("Car is Low on Oxygen Contacting Help.")
-        sendsms()
+        sendsms ()
         openwindow()
         s = 1
     if o > 19:
         s = 0
-        
     
+    if result.temperature > 28 and k == 0:
+        print(" Temp too High")
+        openwindow ()
+        sendsms()
+        k = 1
+    if result.temperature < 28:
+        k = 0
+    time.sleep(3)
